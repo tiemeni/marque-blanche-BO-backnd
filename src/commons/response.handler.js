9e4ac63b804f4870ad53b3aa4402ea7b45bc981c
tiemeni <tiemanirocket@gmail.com>
2023-07-31 15:33:43 +0100

    update register for user

--- src/commons/response.handler.js
+++ src/commons/response.handler.js
@@ -1,3 +1,5 @@
+const { env } = require("../config/env/variables");
+const { COOKIE_NAME } = require("./constants");
 
 exports.successHandler = (res, data, statusCode = 200) => {
     if(data?.access_token) res.cookie(COOKIE_NAME, data.access_token, { maxAge: env.EXPIRE_DATE })
