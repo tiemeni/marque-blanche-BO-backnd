26658e0b0c534c7caab4b4078216c2991868530c
MigueldD143 <78732195+MigueldD143@users.noreply.github.com>
2023-08-11 18:38:37 +0100

    Update appointment.service.js
    populate on practitioner when take rdv

--- src/services/appointment.service.js
+++ src/services/appointment.service.js
@@ -3,7 +3,9 @@ const Appointment = require("../database/models/appointment.model")
 module.exports = {
     createAppointment: async (data) => {
         let appointement = new Appointment(data)
-        return await appointement.save()
+        await appointement.save()
+        
+        return await appointment.populate('practitioner').execPopulate();
     },
     findAndGroup: async () => {
         return await Appointment.aggregate([
