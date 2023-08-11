6ebbf77cc3e3876efbaed073640b31509e01b256
MigueldD143 <78732195+MigueldD143@users.noreply.github.com>
2023-08-11 14:52:22 +0100

    Update patient.controller.js
    retourner l'identifiant de la fiche si elle existe

--- src/database/controllers/patient.controller.js
+++ src/database/controllers/patient.controller.js
@@ -8,7 +8,7 @@ const createPatient = async (req, res) => {
     try {
         // if user patient exists
         const isPatientExist = await patientService.findOneByQuery({ email: data.email, idCentre: req.idCentre })
-        if (isPatientExist) return handler.errorHandler(res, "Patient already exists", httpStatus.BAD_REQUEST)
+        if (isPatientExist) return handler.errorHandler(res, isPatientExist._id, httpStatus.BAD_REQUEST)
 
         //create and store the new patient
         const result = await patientService.createPatient({ ...data, idCentre: req.idCentre });
