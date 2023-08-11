206993d266870f341d9d0883c75427b898599ce6
MigueldD143 <78732195+MigueldD143@users.noreply.github.com>
2023-08-11 17:10:12 +0100

    Update center.middleware.js

--- src/middlewares/center.middleware.js
+++ src/middlewares/center.middleware.js
@@ -5,15 +5,17 @@ const { findCentreById } = require('../services/structure.service')
 module.exports = async (req, res, next) => {
     if ((!req.query.idCentre || req.query.idCentre == null) && !req.query.module )
         return errorHandler(res, "Veuillez renseigner l'identifiant du centre", httpStatus.OK)
-    
-    if(req.query.module === 'externe')
+
+    if ((!req.query.idCentre || req.query.idCentre == null) && req.query.module )
         next();
+    
+    if(req.query.idCentre) {
+        const { idCentre } = req.query;
+        const isExist = await findCentreById(idCentre);
 
-    const { idCentre } = req.query;
-    const isExist = await findCentreById(idCentre);
+        if (!isExist) errorHandler(res, "Veuillez renseigner un identifiant de centre valide", httpStatus.OK)
 
-    if (!isExist) errorHandler(res, "Veuillez renseigner un identifiant de centre valide", httpStatus.OK)
-
-    req.idCentre = idCentre;
-    next()
+        req.idCentre = idCentre;
+        next()
+    }
 }
