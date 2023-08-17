17129291eeb9075cd63b2b6e265c12b1951e5066
MigueldD143 <78732195+MigueldD143@users.noreply.github.com>
2023-08-17 12:11:41 +0100

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,17 +1,6 @@
 const router = require('express').Router();
 const userController = require('../database/controllers/user.controller');
 const auth = require('../middlewares/auth.middleware')
-const multer = require('multer')
-const path = require('path')
-
-const storage = multer.diskStorage({
-    destination: '/src/uploads/', // Répertoire où les fichiers seront enregistrés
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    },
-});
-const upload = multer({ storage });
 
 // GET
 router.get('/', userController.getAllUsers);
@@ -25,7 +14,6 @@ router.post('/register', userController.createUser);
 
 // PATCH
 router.patch('/:userid', userController.updateUserById);
-router.patch('/upload-photo/:userid', upload.single('photo'), userController.uploadPicture)
 
 // DELETE
 router.delete('/', userController.deleteAllUsers);
