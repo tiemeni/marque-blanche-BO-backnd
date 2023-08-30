c6da813d62be170705177d120f7f5edde04de291
tiemeni <81453256+tiemeni@users.noreply.github.com>
2023-08-30 14:42:37 +0100

    Update user.controller.js

--- src/database/controllers/user.controller.js
+++ src/database/controllers/user.controller.js
@@ -192,7 +192,7 @@ const processVerifCode = async (req, res) => {
             }
             const result = await sendCodeVerif(codeVerif, email, callbacks);
         } else {
-            return handler.errorHandler(res, err, "user doesn't exist!")
+            return handler.errorHandler(res, "email doesn't exist!", 404)
         }
     } catch (error) {
         return handler.errorHandler(res, error, httpStatus.INTERNAL_SERVER_ERROR)
