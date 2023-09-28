5e406d658c95b43fa1ac591cf6e5a0230ec0ab54
MigueldD143 <dongmodonald95@gmail.com>
2023-09-28 17:56:55 +0100

    setup socket inside server and appointment

--- src/services/appointment.service.js
+++ src/services/appointment.service.js
@@ -1,84 +1,90 @@
-const Appointment = require("../database/models/appointment.model")
+const Appointment = require("../database/models/appointment.model");
 
 module.exports = {
-    createAppointment: async (data) => {
-        let appointment = new Appointment(data)
-        await appointment.save()
+  createAppointment: async (data) => {
+    let appointment = new Appointment(data);
+    await appointment.save();
 
-        return await appointment.populate('practitioner');
-    },
-    findAndGroup: async () => {
-        return await Appointment.aggregate([
-            {
-                $group: {
-                    _id: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
-                    events: { $push: '$$ROOT' }
-                }
-            },
-            {
-                $sort: { _id: 1 }
-            }
-        ])
-    },
-    findOneByQuery: async (query) => {
-        return await Appointment.findOne(query)
-    },
-    findByQuery: async (query) => {
-        return await Appointment
-            .find(query)
-            .select("-patient")
-            .populate({
-                path: "motif",
-                select: "-active -default_time"
-            })
-            .populate({
-                path: "patient",
-                select: "-rights -active -password -civility"
-            })
-            .populate("center")
-            .populate("lieu")
-            .populate({
-                path: "practitioner",
-                populate: [{
-                    path: "civility",
-                    model: "Civilities",
-                    select: "-password"
-                }, {
-                    path: "job",
-                    model: "Specialite",
-                    select: "title"
-                }]
-            })
-    },
-    findAll: async (query) => {
-        return await Appointment.find(query)
-    },
-    deleteAll: async () => {
-        return await Appointment.deleteMany({})
-    },
-    editeOneByQuery: async (id, idc, query) => {
-        return await Appointment.findOneAndUpdate({ _id: id }, query, { new: true })
-            .populate({
-                path: "patient",
-            })
-            .populate({
-                path: "motif",
-                select: "-default_time"
-            })
-            .populate({
-                path: "practitioner",
-                populate: [{
-                    path: "civility",
-                    model: "Civilities",
-                    select: "-password"
-                }, {
-                    path: "job",
-                    model: "Specialite",
-                    select: "title"
-                }]
-            })
-    },
-    findAndDelete: async (id, query) => {
-        return await Appointment.findByIdAndDelete(id, query)
-    }
-}
+    return await appointment.populate("practitioner");
+  },
+  findAndGroup: async () => {
+    return await Appointment.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+          events: { $push: "$$ROOT" },
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+    ]);
+  },
+  findOneByQuery: async (query) => {
+    return await Appointment.findOne(query);
+  },
+  findByQuery: async (query) => {
+    return await Appointment.find(query)
+      .select("-patient")
+      .populate({
+        path: "motif",
+        select: "-active -default_time",
+      })
+      .populate({
+        path: "patient",
+        select: "-rights -active -password -civility",
+      })
+      .populate("center")
+      .populate("lieu")
+      .populate({
+        path: "practitioner",
+        populate: [
+          {
+            path: "civility",
+            model: "Civilities",
+            select: "-password",
+          },
+          {
+            path: "job",
+            model: "Specialite",
+            select: "title",
+          },
+        ],
+      })
+      .sort({ date_long: 1 });
+  },
+  findAll: async (query) => {
+    return await Appointment.find(query);
+  },
+  deleteAll: async () => {
+    return await Appointment.deleteMany({});
+  },
+  editeOneByQuery: async (id, idc, query) => {
+    return await Appointment.findOneAndUpdate({ _id: id }, query, { new: true })
+      .populate({
+        path: "patient",
+      })
+      .populate({
+        path: "motif",
+        select: "-default_time",
+      })
+      .populate({
+        path: "practitioner",
+        populate: [
+          {
+            path: "civility",
+            model: "Civilities",
+            select: "-password",
+          },
+          {
+            path: "job",
+            model: "Specialite",
+            select: "title",
+          },
+        ],
+      });
+  },
+  findAndDelete: async (id, query) => {
+    return await Appointment.findByIdAndDelete(id, query);
+  },
+};
